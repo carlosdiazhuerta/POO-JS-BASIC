0db0f783c787ddcad367394bce6a0724712c4ed7
Francisco Diaz <franciscodiazhuerta97@gmail.com>
2023-05-04 03:39:52 -0500

    modulos

---
+++ module.mjs
@@ -0,0 +1,18 @@
+import { platziClass } from "./main.mjs";
+
+const clase01 = new platziClass({
+name: "javascript: orientada a objetos",
+videoId: "123123456",
+});
+
+clase01.pausar();
+clase01.reproducir();
+
+clase01.pausar();
+clase01.reproducir();
+
+clase01.pausar();
+clase01.reproducir();
+
+clase01.pausar();
+clase01.reproducir();
