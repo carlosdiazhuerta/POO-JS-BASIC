406c393e4d57cc1bd00005e7bde387b82ef90279
Francisco Diaz <franciscodiazhuerta97@gmail.com>
2023-05-11 16:47:11 -0600

    recursividad

--- POO-Intermedio.js
+++ POO-Intermedio.js
@@ -11,6 +11,39 @@ Object.keys(juan);
 Object.getOwnPropertyNames(juan);
 Object.entries(juan);
 
-console.log(Object.keys(juan))
-console.log(Object.getOwnPropertyNames(juan))
-console.log(Object.entries(juan))
+const obj1 = {
+    a: "a",
+    b: "b",
+    c: {
+        d: "d",
+        e: "e",
+    },
+}
+
+const stringfiedComplexObj = JSON.stringify(obj1) // este metodo convierte el objeto en un string
+console.log(stringfiedComplexObj)
+const obj2 = JSON.parse(stringfiedComplexObj) // este metodo convierte el string en un objeto
+console.log(obj2)
+// esta es una forma de copiar objetos en JS, se evita que se haga una copia de la referencia en memoria
+// se realiza una copia en la memoria Heap
+// tanto parse como stringfied no saben trabajar con metodos dentro de objetos
+
+// recursividad
+
+// function recursividad(numero){
+//     console.log(numero)
+//     if(numero<5){
+//         return recursividad(numero + 1);
+//     } else {
+//         return 5;
+//     }
+// };
+const arrleglo = ["2", "22", "32", "43", "55", "100", "lala", "510"];
+function recursividad(numArry){
+    if(numArry != 0){
+        const firstNum = numArry[0];
+        console.log(firstNum);
+        numArry.shift();
+        recursividad(numArry);
+    }
+}
